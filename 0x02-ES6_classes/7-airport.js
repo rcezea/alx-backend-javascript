8df59ce7e4864de348191745e9bbfd1c022fe6c3
Richard <rcezea007@gmail.com>
2024-04-26 04:21:23 +0100

    task 7

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,34 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw TypeError;
+    }
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
