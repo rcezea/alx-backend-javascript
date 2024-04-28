15162b4b21298b97864b5c6cca61fe2410dc123e
Richard <rcezea007@gmail.com>
2024-04-28 22:50:23 +0100

    Practice

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,19 +1,19 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    let str = '';
+  let str = '';
 
-    return uploadPhoto()
-        .then((resp) => {
-            str += `${resp.body} `;
-            return createUser();
-        })
-        .then((resp) => {
-            str += `${resp.firstName} ${resp.lastName}`;
-            console.log(str);
-        })
-        .catch(() => {
-            console.log('Signup system offline');
-            return str;
-        });
+  return uploadPhoto()
+    .then((resp) => {
+      str += `${resp.body} `;
+      return createUser();
+    })
+    .then((resp) => {
+      str += `${resp.firstName} ${resp.lastName}`;
+      console.log(str);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+      return str;
+    });
 }
