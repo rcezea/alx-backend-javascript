29935ce7740178e010991daab6f3929ee8c87b1a
Richard <rcezea007@gmail.com>
2024-04-29 00:30:31 +0100

    practice

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,20 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-//   const user = signUpUser(firstName, lastName);
-//   const photo = uploadPhoto(fileName);
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const newArray = [];
-  const results = await
-  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  for (const result of results) {
-    if (result.status === 'fulfilled') {
-      newArray.push(result);
-    } else {
-      // eslint-disable-next-line no-param-reassign
-      result.value = `Error: ${result.reason.message}`;
-      delete result.reason;
-      newArray.push(result);
-    }
-  }
+  Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then(([result, result2]) => {
+      if (result.status === 'fulfilled') {
+        newArray.push(result);
+      }
+      if (result2.status !== 'fulfilled') {
+        // eslint-disable-next-line no-param-reassign
+        result2.value = `Error: ${result2.reason.message}`;
+        // eslint-disable-next-line no-param-reassign
+        delete result.reason;
+        newArray.push(result);
+      }
+    });
   return newArray;
 }
