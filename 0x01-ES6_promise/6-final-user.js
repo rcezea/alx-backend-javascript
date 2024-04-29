a7781e2d4718b2cc9c04bca3c516bf1ab08377a5
Richard <rcezea007@gmail.com>
2024-04-29 03:10:19 +0100

    practice

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,12 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ]).then(([userResult, photoResult]) => {
-    const userStatus = userResult.status;
-    const photoStatus = photoResult.status;
-
-    return [
-      { status: userStatus, value: userResult.value},
-      { status: photoStatus, value: `Error: ${photoResult.reason.message}` }
-    ];
-  })
+    uploadPhoto(fileName),
+  ]).then(([userResult, photoResult]) => [
+    { status: userResult.status, value: userResult.value },
+    { status: photoResult.status, value: `Error: ${photoResult.reason.message}` },
+  ]);
 }
