88c289068dfad9cad5bf9b9dc9fd74ddbde2dd7e
Richard <rcezea007@gmail.com>
2024-04-29 02:36:22 +0100

    practice

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
@@ -10,18 +10,8 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     const photoStatus = photoResult.status === 'fulfilled' ? 'fulfilled' : 'rejected';
 
     return [
-      {
-        user: {
-          status: userStatus,
-          value: userStatus === 'fulfilled' ? userResult.value : `Error: ${userResult.reason.message}`
-        }
-      },
-      {
-        photo: {
-          status: photoStatus,
-          value: photoStatus === 'fulfilled' ? photoResult.value : `Error: ${photoResult.reason.message}`
-        }
-      }
+      { status: userStatus, value: userResult.value},
+      { status: photoStatus, value: photoStatus === 'fulfilled' ? photoResult.value : `Error: ${photoResult.reason.message}` }
     ];
-  });
+  })
 }
