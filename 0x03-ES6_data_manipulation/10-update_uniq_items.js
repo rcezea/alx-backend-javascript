07be7a5424693e7894f56ce30d75dac17e24ed99
Richard <rcezea007@gmail.com>
2024-04-30 06:07:33 +0100

    task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,15 +3,9 @@ export default function updateUniqueItems(items) {
     throw new Error('Cannot process');
   }
 
-  const updatedItems = new Map();
-
   for (const [key, value] of items.entries()) {
     if (value === 1) {
-      updatedItems.set(key, 100);
-    } else {
-      updatedItems.set(key, value);
+      items.set(key, 100);
     }
   }
-
-  return updatedItems;
 }
