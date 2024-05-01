963de799ff94715fa81d981254cd46d2de456a4a
Richard <rcezea007@gmail.com>
2024-05-01 05:54:14 +0100

    Advanced task 0

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -7,7 +7,7 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   if (weakMap.get(endpoint) >= 5) {
-    throw Error('Endpoint load is high');
+    throw new Error('Endpoint load is high');
   }
   weakMap.set(endpoint, (weakMap.get(endpoint) || 0) + 1);
 }
