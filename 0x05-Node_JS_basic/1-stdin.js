0528f9853ecf9f53173de1040b9a2ec1ff1a9d77
Richard <rcezea007@gmail.com>
2024-06-17 07:53:40 +0100

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,15 @@
-const readline = require('node:readline');
+const process = require('process');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+console.log('Welcome to Holberton School, what is your name?');
 
-rl.question('Welcome to Holberton School, what is your name?\n', name => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
-});
-
-rl.on('SIGINT', () => {
-
-  console.log('This important software is now closing\n');
-  rl.close();
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
