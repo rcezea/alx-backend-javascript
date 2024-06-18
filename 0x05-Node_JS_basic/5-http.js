d02173946323b67d15e5098fa97c0f463b702ff6
Richard <rcezea007@gmail.com>
2024-06-18 21:09:45 +0100

    task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,21 +3,16 @@ const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    const databasePath = process.argv[2];
     const msg = 'This is the list of our students\n';
-
-    countStudents(databasePath)
-      .then((data) => {
-        res.end(`${msg + data}`);
-      })
-      .catch(() => {
-        res.statusCode = 500;
-        res.end(`${msg}Cannot load the database`);
-      });
+    countStudents(process.argv[2]).then((data) => {
+      res.end(msg + data);
+    }).catch(() => {
+      res.statusCode = 500;
+      res.end(`${msg}Cannot load the database`);
+    });
   }
 });
 
