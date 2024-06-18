c5d0dba0fecd1985a4a47f5f5eac7098e3a45ecb
Richard <rcezea007@gmail.com>
2024-06-18 20:31:08 +0100

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,35 @@
 const fs = require('fs');
+const readline = require('readline');
 
-function countStudents (path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
+function countStudents(dbName) {
+  if (fs.existsSync(dbName)) {
+    const stream = fs.createReadStream(dbName);
+    const reader = readline.createInterface({ input: stream });
 
-    if (!data) {
-      throw new Error('Cannot load the database');
-    }
+    const data = new Map();
+    let numOfStudent = -1;
 
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-
-    const students = {};
-    let totalStudents = 0;
-
-    lines.forEach((line, index) => {
-      if (index === 0) return;
-
-      const [firstname, , , field] = line.split(',');
-
-      if (firstname && field) {
-        if (!students[field]) {
-          students[field] = [];
+    reader.on('line', (row) => {
+      numOfStudent += 1;
+      if (numOfStudent > 0) {
+        const rowData = row.split(',');
+        if (!data.get(rowData[3])) {
+          data.set(rowData[3], [rowData[0]]);
+        } else {
+          const curNames = data.get(rowData[3]);
+          curNames.push(rowData[0]);
         }
-        students[field].push(firstname);
-        totalStudents++;
       }
     });
 
-    console.log(`Number of students: ${totalStudents}`);
-    for (const [field, names] of Object.entries(students)) {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-    }
-  } catch (error) {
+    reader.on('close', () => {
+      console.log(`Number of students: ${numOfStudent}`);
+      for (const corseAndNames of data.entries()) {
+        const msg = `Number of students in ${corseAndNames[0]}: ${corseAndNames[1].length}. List: ${corseAndNames[1].join(', ')}`;
+        console.log(msg);
+      }
+    });
+  } else {
     throw new Error('Cannot load the database');
   }
 }
